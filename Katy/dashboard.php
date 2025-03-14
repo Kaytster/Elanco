69bee70c4bbc17d57731ccf0315dd087254bd834
Kaytster <ktsharps@gmail.com>
2025-03-14 11:48:19 +0000

    Started Dashbaord

---
+++ Katy/dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="dashboard.css">
+	<body>
+		<div class="row">
+			<div class="coldate">
+				<h3>01/01/2021</h3>
+			</div>
+			<div class="colname">
+				<h3>Snoopy</h3>
+			</div>
+			<div class="colID">
+				<h3>CANINE001</h3>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="colweight">
+				<h3>Weight (kg)</h3>
+				<p>7.2kg</p>
+			</div>
+			<div class="colbehav">
+				<h3>Behaviour Pattern</h3>■
+				<p><span style="color:pink;">■■</span><span style="color:green;">■■■■■■■</span><span style="color:purple;">■</span><span style="color:yellow;">■</span>
+				<span style="color:green;">■</span><span style="color:pink;">■■■■</span><span style="color:purple;">■</span><span style="color:green;">■</span>
+				<span style="color:pink;">■</span><span style="color:yellow;">■</span><span style="color:pink;">■■</span><span style="color:green;">■■</span></p>
+			</div>
+			<div class="colbark">
+				<h3>Barking Frequency</h3>
+			</div>
+		</div>
+	</body>
+</html>
