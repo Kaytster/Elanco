05c28786f679cdcbdafefb3b392a4ff486d9a030
Kaytster <ktsharps@gmail.com>
2025-03-26 15:47:54 +0000

    Date picker added

--- Katy/dashboard.php
+++ Katy/dashboard.php
@@ -6,63 +6,72 @@ try {
     // echo "Connected to the SQLite3 database!";
 
     
+
+	$dateID = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
+	$query = $db->prepare("SELECT strftime('%d-%m-%Y', ?)");
+    $query->bindValue(1, $dateID);
+    $result = $query->execute();
+    $row = $result->fetchArray(SQLITE3_NUM);
+    $formattedDate = $row[0];
+
+    // echo "Selected Date: " . $formattedDate . "<br>";
+	
     
     $dogID = $db->query('SELECT Dog_ID FROM Activity WHERE Dog_ID="CANINE001" LIMIT 1 ');
 	$rowDOG = $dogID->fetchArray(SQLITE3_ASSOC); // Get the data
 
-	$weightID = $db->query('SELECT round(avg(Weight), 1) AS "Weight_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
+	$weightID = $db->query("SELECT round(avg(Weight), 1) AS 'Weight_ID' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate'");
 	$rowWEIGHT = $weightID->fetchArray(SQLITE3_ASSOC);
 
-	// $behavID = $db->query('SELECT avg(Behaviour_ID) AS "Behaviour_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
-	// $rowBEHAV = $behavID->fetchArray(SQLITE3_ASSOC);
-
-	$normalID = $db->query('SELECT count(Behaviour_ID) AS "Normal" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021" AND Behaviour_ID="1"');
+	$normalID = $db->query("SELECT count(Behaviour_ID) AS 'Normal' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate' AND Behaviour_ID='1'");
 	$rowNORMAL = $normalID->fetchArray(SQLITE3_ASSOC);
 
-	$walkingID = $db->query('SELECT count(Behaviour_ID) AS "Walking" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021" AND Behaviour_ID="2"');
+	$walkingID = $db->query("SELECT count(Behaviour_ID) AS 'Walking' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate' AND Behaviour_ID='2'");
 	$rowWALKING = $walkingID->fetchArray(SQLITE3_ASSOC);
 
-	$eatingID = $db->query('SELECT count(Behaviour_ID) AS "Eating" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021" AND Behaviour_ID="3"');
+	$eatingID = $db->query("SELECT count(Behaviour_ID) AS 'Eating' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate' AND Behaviour_ID='3'");
 	$rowEATING = $eatingID->fetchArray(SQLITE3_ASSOC);
 
-	$sleepingID = $db->query('SELECT count(Behaviour_ID) AS "Sleeping" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021" AND Behaviour_ID="4"');
+	$sleepingID = $db->query("SELECT count(Behaviour_ID) AS 'Sleeping' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate' AND Behaviour_ID='4'");
 	$rowSLEEPING = $sleepingID->fetchArray(SQLITE3_ASSOC);
 
-	$playingID = $db->query('SELECT count(Behaviour_ID) AS "Playing" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021" AND Behaviour_ID="5"');
+	$playingID = $db->query("SELECT count(Behaviour_ID) AS 'Playing' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate' AND Behaviour_ID='5'");
 	$rowPLAYING = $playingID->fetchArray(SQLITE3_ASSOC);
 
-	$barkID = $db->query('SELECT round(avg(Frequency_ID), 1) AS "Frequency_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
+	$barkID = $db->query("SELECT round(avg(Frequency_ID), 1) AS 'Frequency_ID' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate'");
 	$rowBARK = $barkID->fetchArray(SQLITE3_ASSOC);
 
+	$barkingFREQ = "";
+
 	if ($rowBARK['Frequency_ID'] < 2) {
-		$rowBARK="None";
+		$barkingFREQ="None";
 	} elseif ($rowBARK['Frequency_ID'] >= 2 AND $rowBARK['Frequency_ID'] < 3 ) {
-		echo "Low";
+		$barkingFREQ="Low";
 	} elseif ($rowBARK['Frequency_ID'] >= 3 AND $rowBARK['Frequency_ID'] < 4) {
-		echo "Medium";
+		$barkingFREQ="Medium";
 	} elseif ($rowBARK['Frequency_ID'] >= 4 AND $rowBARK['Frequency_ID'] < 5) {
-		echo "High";
+		$barkingFREQ="High";
 	}
 
-	$stepsID = $db->query('SELECT sum(Activity_Level) AS "Steps_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
+	$stepsID = $db->query("SELECT sum(Activity_Level) AS 'Steps_ID' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate'");
 	$rowSTEPS = $stepsID->fetchArray(SQLITE3_ASSOC);
 
-	$heartID = $db->query('SELECT round(avg(Heart_Rate), 1) AS "Heart_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
+	$heartID = $db->query("SELECT round(avg(Heart_Rate), 1) AS 'Heart_ID' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate'");
 	$rowHEART = $heartID->fetchArray(SQLITE3_ASSOC);
 
-	$tempID = $db->query('SELECT round(avg(Temperature), 1) AS "Temp_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
+	$tempID = $db->query("SELECT round(avg(Temperature) , 1) AS 'Temp_ID' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate'");
 	$rowTEMP = $tempID->fetchArray(SQLITE3_ASSOC);
 
-	$breathID = $db->query('SELECT round(avg(Breath_Rate), 1) AS "Breath_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
+	$breathID = $db->query("SELECT round(avg(Breath_Rate) , 1) AS 'Breath_ID' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate'");
 	$rowBREATH = $breathID->fetchArray(SQLITE3_ASSOC);
 
-	$foodID = $db->query('SELECT sum(Food_Intake) AS "Food_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
+	$foodID = $db->query("SELECT sum(Food_Intake) AS 'Food_ID' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate'");
 	$rowFOOD = $foodID->fetchArray(SQLITE3_ASSOC);
 
-	$calID = $db->query('SELECT sum(Calorie_Burnt) AS "Cal_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
+	$calID = $db->query("SELECT sum(Calorie_Burnt) AS 'Cal_ID' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate'");
 	$rowCAL = $calID->fetchArray(SQLITE3_ASSOC);
 
-	$waterID = $db->query('SELECT sum(Water_Intake) AS "Water_ID" FROM Activity WHERE Dog_ID="CANINE001" AND D_Date="01-01-2021"');
+	$waterID = $db->query("SELECT sum(Water_Intake) AS 'Water_ID' FROM Activity WHERE Dog_ID='CANINE001' AND D_Date='$formattedDate'");
 	$rowWATER = $waterID->fetchArray(SQLITE3_ASSOC);
 	
 } catch (Exception $e) {
@@ -87,109 +96,12 @@ src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
 </script>
 
 	<body>
+		<!-- Widgets -->
 		<section>
-
-			<!-- <div class="row">
-				<div class="coldate">
-					<h3>01/01/2020</h3>
-				</div>
-				<div class="colname">
-					<h3>Snoopy</h3>
-				</div>
-				<div class="colID">
-					<h3>CANINE001</h3>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="colweight">
-					<h3>Weight (kg)</h3>
-					<p style="font-family: Arial, Helvetica, sans-serif; font-size: 35px;">7.2kg</p>
-				</div>
-				<div class="colbehav">
-					<h3>Behaviour Pattern</h3>
-				</div>
-				<div class="colbark">
-					<h3>Barking Frequency</h3>
-					<img src="Frequency.png" alt="Frequency" width="150" height="150"> 
-					<p style="font-family: Arial, Helvetica, sans-serif; font-size: 35px;">High</p>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="colactivity">
-					<h3>Activity Level (steps)</h3>
-					<svg width="150" height="150" viewBox="0 0 250 250"
-				 		class="activity-progress" style="--progress: 60"
-					>
-						<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">7960 Steps</text>    
-						<circle class="bgA"></circle>
-						<circle class="fgA"></circle>
-					</svg>
-				</div>
-				<div class="colheart">
-					<h3>Heart Rate (bpm)</h3>
-					<svg width="150" height="150" viewBox="0 0 250 250"
-				 		class="heart-progress" style="--progress: 25"
-					>
-						<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">110 BPM</text>    
-						<circle class="bgH"></circle>
-						<circle class="fgH"></circle>
-					</svg>
-				</div>
-				<div class="coltemp">
-					<h3>Temperature (C)</h3>
-					<svg width="150" height="150" viewBox="0 0 250 250"
-				 		class="temp-progress" style="--progress: 25"
-					>
-						<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">27.2C</text>    
-						<circle class="bgT"></circle>
-						<circle class="fgT"></circle>
-					</svg>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="colbreath">
-					<h3>Breathing Rate</h3>
-					<h3>(breaths/min)</h3>
-					<svg width="150" height="150" viewBox="0 0 250 250"
-				 		class="breath-progress" style="--progress: 50"
-					>
-						<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">20</text>    
-						<circle class="bgB"></circle>
-						<circle class="fgB"></circle>
-					</svg>
-				</div>
-				<div class="colfood">
-					<h3>Food Intake (calories)</h3>
-					<div class="container">
-						<img src="Food.png" alt="Food" width="150" height="150"> 
-						<div class="centered">261.3</div>
-					</div>
-				</div>
-				<div class="colcalorie">
-					<h3>Calorie Burn</h3>
-					<div class="container">
-						<img src="Food.png" alt="Foof" width="150" height="150"> 
-						<div class="centered">278.4</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="colwater">
-					<h3>Water Intake (ml)</h3>
-					<div class="container">
-						<img src="Water.png" alt="Water" width="150" height="150"> 
-						<div class="centered">458.1</div>
-					</div>
-				</div>
-			</div> -->
 		<div class="row">
 				<div class="card" style="background-color: #ecf6fd; width: 28%; margin-left:10px; text-align:center; border-color:#0067B1; border-width:1px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
 					<div class="card-body">
-						<h3 class="card-title">01/01/2021</h3>
+						<h3 class="card-title"><?php echo $formattedDate;?></h3>
 					</div>
 				</div>
 			<div class="card" style="background-color: #ecf6fd; width: 28%; margin-left:60px; text-align:center; border-color:#0067B1; border-width:1px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
@@ -204,6 +116,25 @@ src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
 			</div>
 		</div>
 
+		<!-- Date Picker -->
+		<section>
+			<div class="row">
+				<div class="card" style="background-color: #ecf6fd; width: 28%; border-color:#0067B1; border-width:1px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); margin-left:10px;">
+					<div class="card-body">
+						<h2 class="text-primary">Date Selector</h2>
+						<input type="date" id="date" class="form-control date-picker" value="<?= date('Y-m-d') ?>">
+					</div>
+					<script>
+                        document.getElementById('date').addEventListener('change', function() {
+                            var selectedDate = this.value;
+                            //alert(selectedDate);
+                            window.location.href = '?date=' + selectedDate;
+                        });
+                    </script>
+				</div>
+			</div>
+		</section>
+
 		<div class="row">
 			<div class="card" style="background-color: #ecf6fd; width: 28%; border-color:#0067B1; border-width:1px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); margin-left:10px;">
 				<a href="Weight_chart.php">
@@ -213,12 +144,6 @@ src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
 					</div>
 				</a>
 			</div>
-			<!-- <div class="card" style="background-color: #ecf6fd; width: 28%; border-color:#0067B1; border-width:1px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); margin-left:60px;">
-				<div class="card-body">
-					<h3 class="card-title" style="text-decoration: underline;">Behaviour Pattern</h3>
-					<p class="card-text" style="text-align: center; font-size: 35px;"><?php echo $rowBEHAV['Behaviour_ID']?></p>
-				</div>
-			</div> -->
 
 			<div class="card" style="background-color: #ecf6fd; width: 28%; border-color:#0067B1; border-width:1px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); margin-left:60px;">
 				<div class="card-body">
@@ -259,7 +184,7 @@ src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
 				<div class="card-body">
 					<h3 class="card-title" style="text-decoration: underline;">Barking Frequency</h3>
 						<img src="Frequency.png" alt="Frequency" width="150" height="150">
-					<p class="card-text" style="text-align: center; font-size: 35px;"><?php echo $rowBARK?></p>
+					<p class="card-text" style="text-align: center; font-size: 35px;"><?php echo $barkingFREQ?></p>
 				</a>
 				</div>
 			</div>
@@ -359,6 +284,7 @@ src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
 
 		</section>
 
+		<!-- Pie Chart -->
 		<section>
 		<canvas id="myChart" style="width:100%; margin:auto;"></canvas>
 			<script>
